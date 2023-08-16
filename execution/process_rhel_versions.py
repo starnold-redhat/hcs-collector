81cada4b005118a9ae40ccbf7b92f6e1c259170d
starnold <starnold@redhat.com>
2023-08-16 13:43:41 +0100

    working rhel versions

--- execution/process_rhel_versions.py
+++ execution/process_rhel_versions.py
@@ -13,32 +13,26 @@ def process_rhel_versions(path_to_csv_dir, csv_files_list, tag):
     CURRENT_TIMEFRAME_YEAR = path_to_csv_dir.split("/")[4]
     CURRENT_TIMEFRAME_MONTH = path_to_csv_dir.split("/")[5]
     CURRENT_TIMEFRAME = CURRENT_TIMEFRAME_YEAR + "-" + CURRENT_TIMEFRAME_MONTH
+    OS_VERSIONS = ['5','6','7','8','9']
 
     # max values for the month
-    max_versions=[]
-    max_by_tag = []
-    for major_version in range(5,10):
-        max_versions.append({"version": major_version, "count": 0})
-        max_by_tag.append("version": major_version, "tags": [])
+    max_versions={}
+    max_versions_by_tag = {}
 
     for sheet in csv_files_list:
 
         #counted values for this sheet/day
-        stage_rhel_physical = 0
-        stage_rhel_virtual = 0
-        stage_rhel_vdc = 0
-        stage_unknown = 0
-        stage_by_tag = {}
-        
+        stage_versions={}
+        stage_versions_by_tag = {}
 
         with open(path_to_csv_dir + "/" + sheet, "r") as file_obj:
             csv_file = csv.reader(file_obj)
             
             for row in csv_file:
                 # print(row)
-                infrastructure_type = row[21]
                 installed_product = row[35]
-                hypervisor_fqdn = row[38]
+                os_version = row[17]
+                major_os = os_version[0]
                 vmtags = row[len(row)-1]
                 tagvalue=""
                 if (tag != "none"):
@@ -48,72 +42,61 @@ def process_rhel_versions(path_to_csv_dir, csv_files_list, tag):
 
                 if ('69' in installed_product) or ('479' in installed_product):
                     if (tag != "none" and tagvalue!=""):
-                        count_rhel_value_by_tag(infrastructure_type, stage_by_tag, tagvalue)
+                        count_rhel_version_by_tag(major_os, stage_versions_by_tag, tagvalue)
 
-                    if infrastructure_type == "physical":
-                        stage_rhel_physical = stage_rhel_physical + 1
-                    elif (infrastructure_type == "virtual") and (hypervisor_fqdn.startswith('virt-who-')):
-                        stage_rhel_vdc = stage_rhel_vdc + 1
-                    elif infrastructure_type == "virtual":
-                        stage_rhel_virtual = stage_rhel_virtual + 1
+                    if (major_os in stage_versions):
+                        stage_count = stage_versions.get(major_os).get('count')
+                        stage_versions[major_os]['count'] = stage_count+1
                     else:
-                        stage_unknown = stage_unknown + 1
+                        stage_versions.setdefault(major_os, {'count':1})
+                    
 
         # check whether this day's numbers are bigger than the largest this month so far
-        if stage_rhel_physical > rhel_physical:
-            rhel_physical = stage_rhel_physical 
+        for major_os in OS_VERSIONS:
+            if major_os in max_versions:
+                if (stage_versions[major_os] > max_versions[major_os]):
+                    max_versions[major_os] = stage_versions[major_os]
+            else:
+                max_versions[major_os] = stage_versions[major_os]
 
-        if stage_rhel_virtual > rhel_virtual:
-            rhel_virtual = stage_rhel_virtual
-
-        if stage_rhel_vdc > rhel_vdc:
-            rhel_vdc = stage_rhel_vdc
-
-        if stage_unknown > unknown:
-            unknown = stage_unknown
-        
-        if (tag != "none"):
-            update_max_value_by_tag(stage_by_tag, max_by_tag)
+            if (tag != "none"):
+                update_max_version_by_tag(stage_versions_by_tag, max_versions_by_tag, major_os)
 
 
-    print("Max Concurrent RHEL On-Demand, referrent to ..: {}".format(CURRENT_TIMEFRAME))
-    print("On-Demand, Physical Node .....................: {}".format(rhel_physical))
-    if (tag != "none"):
-        for tagvalue in max_by_tag:
-            util.pretty_print(2,tagvalue, max_by_tag[tagvalue]['physical'])
-    print("On-Demand, Virtual Node ......................: {}".format(rhel_virtual))
-    if (tag != "none"):
-        for tagvalue in max_by_tag:
-            util.pretty_print(2,tagvalue, max_by_tag[tagvalue]['virtual'])
-    print("Virtual Data Center, Virtual Node ............: {}".format(rhel_vdc))
-    print("Unknown ......................................: {}".format(unknown))
+    print("Max Concurrent RHEL On-Demand, by version ....: {}".format(CURRENT_TIMEFRAME))
+    for major_os in OS_VERSIONS:
+        if (major_os in max_versions):
+            print("On-Demand, RHEL " + major_os + ".............................: {}".format(max_versions[major_os]['count']))
+            if (tag != "none"):
+                for tagvalue in max_versions_by_tag:
+                    if (max_versions_by_tag[tagvalue][major_os] >0):
+                        util.pretty_print(2,tagvalue, max_versions_by_tag[tagvalue][major_os])
+    
     print("")
 
 
 
-def update_max_value_by_tag(stage_by_tag, max_by_tag):
-    for tagvalue in stage_by_tag:
-
-        if (tagvalue in max_by_tag):
-            if (stage_by_tag[tagvalue]['physical'] > max_by_tag[tagvalue]['physical']):
-                max_by_tag[tagvalue]['physical'] = stage_by_tag[tagvalue]['physical']
-            if (stage_by_tag[tagvalue]['virtual'] > max_by_tag[tagvalue]['virtual']):
-                max_by_tag[tagvalue]['virtual'] = stage_by_tag[tagvalue]['virtual']
-            if (stage_by_tag[tagvalue]['unknown'] > max_by_tag[tagvalue]['unknown']):
-                max_by_tag[tagvalue]['unknown'] = stage_by_tag[tagvalue]['unknown']
-        else:
-            max_by_tag.setdefault(tagvalue, { 'physical': stage_by_tag[tagvalue]['physical'], 'virtual': stage_by_tag[tagvalue]['virtual'], 'unknown': stage_by_tag[tagvalue]['unknown']})
+def update_max_version_by_tag(stage_by_tag, max_by_tag, major_os):
+    
+    if (major_os.isdigit()):
+        for tagvalue in stage_by_tag:
+    
+            if (tagvalue in max_by_tag):
+                if (stage_by_tag[tagvalue][major_os] > max_by_tag[tagvalue][major_os]):
+                    max_by_tag[tagvalue][major_os] = stage_by_tag[tagvalue][major_os]
+            else:
+                max_by_tag.setdefault(tagvalue, { '5':0, '6': 0, '7': 0, '8':0, '9':0})
+                max_by_tag[tagvalue][major_os] = stage_by_tag[tagvalue][major_os]
             
 
-def count_rhel_value_by_tag(infrastructure_type, stage_by_tag, tagvalue):
-    if (tagvalue in stage_by_tag):
-        tag_summary = stage_by_tag.get(tagvalue)
-    else:
-        tag_summary = stage_by_tag.setdefault(tagvalue, { 'physical':0, 'virtual': 0, 'unknown': 0})
-    if infrastructure_type == "physical":
-        tag_summary['physical'] = tag_summary['physical'] +1
-    elif infrastructure_type == "virtual":
-        tag_summary['virtual'] = tag_summary['virtual'] +1
-    else:
-        tag_summary['unknown'] = tag_summary['unknown'] +1
+def count_rhel_version_by_tag(major_os, versions_by_tag, tagvalue):
+    
+    if (major_os.isdigit()):
+        if (tagvalue in versions_by_tag):
+            tag_summary = versions_by_tag.get(tagvalue)
+        else:
+            tag_summary = versions_by_tag.setdefault(tagvalue, { '5':0, '6': 0, '7': 0, '8':0, '9':0})
+
+        tag_summary[major_os] = tag_summary.get(major_os) +1
+   
 
